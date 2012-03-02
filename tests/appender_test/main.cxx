5c2da4941a02db0e2a77b7959b2dda3b7f35d7d4
Vaclav Zeman <vzeman79@gmail.com>
2012-03-02 22:14:49 +0100

    appender_test/main.cxx: Clarify the comment about log4cplus::initializeLog4cplusAppenders().

--- tests/appender_test/main.cxx
+++ tests/appender_test/main.cxx
@@ -42,10 +42,11 @@ createAppender (tstring const & name)
 int
 main()
 {
-    // Calling initializeLog4cplusAppenders() is necessary to create a
-    // dependency of this executable on log4cplus_appenders
-    // library. Without this dependency the library will not get
-    // loaded and thus no appenders will be registered.
+    // In this test/example, calling initializeLog4cplusAppenders() is
+    // necessary to create a dependency of this executable on
+    // log4cplus_appenders library. Without this dependency the
+    // library will not get loaded and thus no appenders will be
+    // registered.
     log4cplus::initializeLog4cplusAppenders ();
 
     LogLog::getLogLog()->setInternalDebugging(true);
