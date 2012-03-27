5c855d2d2898d4cc3d65c87e51338167c621dfb9
Vaclav Zeman <wilx@users.sourceforge.net>
2012-03-28 00:10:52 +0200

    Implement bind_socket().

--- src/socket-unix.cxx
+++ src/socket-unix.cxx
@@ -570,6 +570,30 @@ create_socket (Socket & sock, AddressFamily af, SocketType st, int proto)
 }
 
 
+template <typename addr_ptr_type, typename socklen_type>
+static
+int
+bind_wrap (
+    int (* bind_func) (int, addr_ptr_type, socklen_type),
+    int socket, sockaddr const * addr, std::size_t addr_len)
+{
+    return bind_func (socket, reinterpret_cast<addr_ptr_type>(addr),
+        static_cast<socklen_type>(addr_len));
+}
+
+
+Error
+bind_socket (Socket const & socket, SockAddr const & addr, std::size_t addr_len)
+{
+    Socket::Data const & sd = socket.get_data ();
+    int ret = bind_wrap (&bind, sd.socket, &addr.get_data ().addr, addr_len);
+    if (ret == 1)
+        return Error (LOG4CPLUS_TEXT ("bind"), EsErrno, errno);
+
+    return Error ();
+}
+
+
 template <typename option_ptr_type, typename socklen_type>
 static
 int
